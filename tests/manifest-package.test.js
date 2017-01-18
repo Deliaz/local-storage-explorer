3268681935baa1e35e9869f5e2bccc03a4cc07b1
Deliaz <D3liaz@gmail.com>
2017-01-19 01:54:49 +0700

    Draft for tests

---
+++ tests/manifest-package.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const fs = require('fs');
+
+describe('manifest.json & package.json', () => {
+    it('manifest.json should be valid JSON file', () => {
+        const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
+        expect(manifestFile).to.be.an.object;
+    });
+
+    it('package.json should be valid JSON file', () => {
+        const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
+        expect(packageFile).to.be.an.object;
+    });
+
+    it('package.json should contain same version number as manifest.json', () => {
+        const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
+        const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
+
+        expect(packageFile.version).to.be.equal(manifestFile.version)
+    });
+});
