24fb97a695743f8b3458c4722d7aa332df41d841
Denis Leonov <del@coccoc.com>
2017-06-16 12:43:11 +0700

    Update test cases: fix breaking changes from mocha 4 (resolve #6). Use Node 8 for tests. Add package-lock.

--- tests/manifest-package.test.js
+++ tests/manifest-package.test.js
@@ -3,20 +3,20 @@ const expect = chai.expect;
 const fs = require('fs');
 
 describe('manifest.json & package.json', () => {
-    it('manifest.json should be valid JSON file', () => {
-        const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
-        expect(manifestFile).to.be.an.object;
-    });
+	it('manifest.json should be valid JSON file', () => {
+		const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
+		expect(manifestFile).to.be.an('object');
+	});
 
-    it('package.json should be valid JSON file', () => {
-        const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
-        expect(packageFile).to.be.an.object;
-    });
+	it('package.json should be valid JSON file', () => {
+		const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
+		expect(packageFile).to.be.an('object');
+	});
 
-    it('package.json should contain same version number as manifest.json', () => {
-        const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
-        const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
+	it('package.json should contain same version number as manifest.json', () => {
+		const manifestFile = JSON.parse(fs.readFileSync(`${__dirname}/../manifest.json`, 'utf-8'));
+		const packageFile = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf-8'));
 
-        expect(packageFile.version).to.be.equal(manifestFile.version)
-    });
+		expect(packageFile.version).to.be.equal(manifestFile.version);
+	});
 });
